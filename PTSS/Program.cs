9c296dc14d953e1827174dc117b97aa25ca93f76
holmanej <holmanej@gmail.com>
2020-05-27 17:28:53 -0400

    Init

---
+++ PTSS/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTSS
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            TSSolver woof = new TSSolver(2, "abc", new string[] { "bc", "a", "aaa" });
+            Stopwatch pTime = new Stopwatch();
+            pTime.Start();
+            Debug.WriteLine(woof.Solve("aaa"));
+            pTime.Stop();
+            Debug.WriteLine(pTime.Elapsed);
+
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ PTSS/TSSolver.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTSS
+{
+    class TSSolver
+    {
+        private Dictionary<char, int> Amap = new Dictionary<char, int>();
+        private Dictionary<int, int[]> P = new Dictionary<int, int[]>();
+        private int M;
+        private int Halt = -1;
+
+        public TSSolver(int M, string A, string[] rules)
+        {
+            this.M = M;
+
+            for (int i = 0; i < A.Count(); i++)
+            {
+                Amap.Add(A[i], i);
+                if (A[i] == 'H')
+                {
+                    Halt = i;
+                }
+            }
+            
+            for (int i = 0; i < rules.Count(); i++)
+            {
+                int[] rule = new int[rules[i].Count()];
+                for (int j = 0; j < rules[i].Count(); j++)
+                {
+                    rule[j] = Amap[rules[i][j]];
+                }
+                P.Add(i, rule);
+            }
+
+            //foreach (var entry in P)
+            //{
+            //    Debug.Write(entry.Key + " = ");
+            //    foreach (int s in entry.Value)
+            //    {
+            //        Debug.Write(s + ", ");
+            //    }
+            //    Debug.Write("\r\n");
+            //}
+        }
+
+
+        public string Solve(string tape)
+        {
+            Queue<int> T = new Queue<int>();
+            foreach (char c in tape)
+            {
+                T.Enqueue(Amap[c]);
+            }
+
+            while (T.Count >= M && T.Peek() != Halt)
+            {
+                foreach (int app in P[T.Dequeue()])
+                {
+                    T.Enqueue(app);
+                }
+                for (int i = 1; i < M; i++)
+                {
+                    T.Dequeue();
+                }
+                //foreach (int s in T)
+                //{
+                //    Debug.Write(Amap.Keys.ElementAt(s));
+                //}
+                //Debug.Write("\r\n");
+            }
+
+            string result = "";
+            foreach (int s in T)
+            {
+                result += Amap.Keys.ElementAt(s);
+            }
+
+            return result;
+        }
+    }
+}
